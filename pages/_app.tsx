5236fedf7bd135f5863b6d34cdae06ce81acb9a2
thewebwelost <thawebwelost@gmail.com>
2022-09-04 17:25:02 -0700

    fixed import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/types/react-query-devtools/src/devtools';
 import type { AppProps } from 'next/app';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
