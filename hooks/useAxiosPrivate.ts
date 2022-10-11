407507f99b7d454a34cd93347effa4fa39b99941
thewebwelost <thawebwelost@gmail.com>
2022-10-10 17:37:56 -0700

    old auth context removed

--- hooks/useAxiosPrivate.ts
+++ hooks/useAxiosPrivate.ts
@@ -1,55 +1,53 @@
-import { useEffect } from 'react';
-import { AxiosRequestConfig } from 'axios';
+// import { useEffect } from 'react';
+// import { AxiosRequestConfig } from 'axios';
 import { axiosPrivate } from '../utils/http/axios';
-import { useAuthContext } from '../context/authContext';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 function useAxiosPrivate() {
-  const authContext = useAuthContext();
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const requestIntercept = axiosPrivate.interceptors.request.use(
-      (config: AxiosRequestConfig): AxiosRequestConfig => {
-        if (config.headers === undefined) {
-          config.headers = {};
-        }
+  // useEffect(() => {
+  //   const requestIntercept = axiosPrivate.interceptors.request.use(
+  //     (config: AxiosRequestConfig): AxiosRequestConfig => {
+  //       if (config.headers === undefined) {
+  //         config.headers = {};
+  //       }
 
-        if (!config.headers['Authorization']) {
-          config.headers[
-            'Authorization'
-          ] = `Bearer ${authContext?.auth?.accessToken}`;
-        }
+  //       // if (!config.headers['Authorization']) {
+  //       //   config.headers[
+  //       //     'Authorization'
+  //       //   ] = `Bearer ${authContext?.auth?.accessToken}`;
+  //       // }
 
-        return config;
-      }
-    );
+  //       return config;
+  //     }
+  //   );
 
-    const responseIntercept = axiosPrivate.interceptors.response.use(
-      (response) => response,
-      async (err) => {
-        console.log('err', err);
-        const prevRequest = err?.config;
-        if (err.response.status === 401) return router.push('/login');
-        if (err.response.status === 403 && !prevRequest?.sent) {
-          try {
-            prevRequest.sent = true;
-            const accessToken = await authContext?.refresh();
-            prevRequest.headers['Authorization'] = `Bearer ${accessToken}`;
-            return axiosPrivate(prevRequest);
-          } catch (err) {
-            return router.push('/login');
-          }
-        }
-        return Promise.reject(err);
-      }
-    );
+  //   const responseIntercept = axiosPrivate.interceptors.response.use(
+  //     (response) => response,
+  //     async (err) => {
+  //       console.log('err', err);
+  //       const prevRequest = err?.config;
+  //       if (err.response.status === 401) return router.push('/login');
+  //       if (err.response.status === 403 && !prevRequest?.sent) {
+  //         try {
+  //           prevRequest.sent = true;
+  //           // const accessToken = await authContext?.refresh();
+  //           prevRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+  //           return axiosPrivate(prevRequest);
+  //         } catch (err) {
+  //           return router.push('/login');
+  //         }
+  //       }
+  //       return Promise.reject(err);
+  //     }
+  //   );
 
-    return () => {
-      axiosPrivate.interceptors.request.eject(requestIntercept);
-      axiosPrivate.interceptors.response.eject(responseIntercept);
-    };
-  }, [router, authContext]);
+  //   return () => {
+  //     axiosPrivate.interceptors.request.eject(requestIntercept);
+  //     axiosPrivate.interceptors.response.eject(responseIntercept);
+  //   };
+  // }, [router, authContext]);
 
   return axiosPrivate;
 }
