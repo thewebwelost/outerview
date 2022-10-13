24b537190ba0b557828824bec825be7db5fd0724
thewebwelost <thawebwelost@gmail.com>
2022-10-12 17:21:41 -0700

    react querry remover for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,12 @@
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import '../styles/globals.css';
 import { AuthContextProvider } from '../context/authContext';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <Component {...pageProps} />
-      </AuthContextProvider>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
   );
 }
 
