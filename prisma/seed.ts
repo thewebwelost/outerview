96ee2ef6b09fe1bc517a4a94c87fe37ac47eec9e
thewebwelost <thawebwelost@gmail.com>
2022-10-28 18:03:00 -0700

    prisma seed extended

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,49 @@ const prisma = new PrismaClient();
 async function main() {
   const user = await prisma.user.create({
     data: {
-      name: 'Alice',
+      name: 'Billy',
       credentials: {
         create: {
-          email: 'test@email.yo',
+          email: 'test2@email.yo',
           password: 'aaaa',
         },
+      },
+      profiles: {
+        create: [{}],
+      },
+      applications: {
+        create: [
+          {
+            company: 'Cheese Ind.',
+            role: 'detective',
+            url: 'https://www.prisma.io/',
+            description: 'Nice job, good people',
+            salary: '',
+            location: '',
+            contacts: {
+              create: [
+                {
+                  title: 'test 1',
+                  url: 'https://www.prisma.io/',
+                },
+                {
+                  title: 'test 2',
+                  url: 'https://www.prisma.io/',
+                },
+              ],
+            },
+            userEvents: {
+              create: [
+                {
+                  dateStart: new Date(),
+                  dateEnd: new Date(),
+                  meetinUrl: 'https://www.prisma.io/',
+                  description: 'Some event',
+                },
+              ],
+            },
+          },
+        ],
       },
     },
     include: {
