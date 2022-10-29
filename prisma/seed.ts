8aace8a87ad675753dbfff0dfa553a1da27b6a73
thewebwelost <thawebwelost@gmail.com>
2022-10-28 17:05:18 -0700

    prisma seed added

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      credentials: {
+        create: {
+          email: 'test@email.yo',
+          password: 'aaaa',
+        },
+      },
+    },
+    include: {
+      credentials: true,
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
