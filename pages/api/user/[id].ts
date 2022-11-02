05f46331ce59166e6f355a9700104bd54d7ad020
thewebwelost <thawebwelost@gmail.com>
2022-11-01 18:40:53 -0700

    minor cleanup

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -2,8 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../prisma/client';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log({ query: req.query });
-
   const user = await prisma.user.findUnique({
     where: {
       id: parseInt(req.query.id as string),
