90fcf60dac1d676373cb38eef37ece51c230a26a
thewebwelost <thawebwelost@gmail.com>
2022-11-02 17:41:17 -0700

    prisma client exceed bug fixed

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,5 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// thanks to this guy https://github.com/prisma/prisma/issues/1983#issuecomment-765317194
+
+const prismaClientPropertyName = `__prevent-name-collision__prisma`;
+type GlobalThisWithPrismaClient = typeof globalThis & {
+  [prismaClientPropertyName]: PrismaClient;
+};
+
+const getPrismaClient = () => {
+  if (process.env.NODE_ENV === `production`) {
+    return new PrismaClient();
+  } else {
+    const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+    if (!newGlobalThis[prismaClientPropertyName]) {
+      newGlobalThis[prismaClientPropertyName] = new PrismaClient();
+    }
+    return newGlobalThis[prismaClientPropertyName];
+  }
+};
+
+const prisma = getPrismaClient();
 
 export default prisma;
