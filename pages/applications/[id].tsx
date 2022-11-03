9273d70de6a6faacdb5358637dd48d546d92d5b2
thewebwelost <thawebwelost@gmail.com>
2022-11-02 17:42:59 -0700

    application route added

--- pages/applications/[id].tsx
+++ pages/applications/[id].tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { IApplication } from '../../components/Application';
+import EventsPanel from '../../components/EventsPanel';
 import Layout from '../../components/Layout';
-
-interface IApplication {}
 
 const Application: NextPage = () => {
   const router = useRouter();
@@ -29,8 +29,28 @@ const Application: NextPage = () => {
   return (
     <Layout>
       <>
-        <h1 className="text-3xl font-bold underline">Single application</h1>
-        {JSON.stringify(application)}
+        <h1 className="mb-3 mt-3 text-3xl font-bold underline">
+          Applications:
+        </h1>
+        <h2 className="mb-3 mt-3 text-xl font-bold">{application?.company}</h2>
+        <p>{application?.role}</p>
+        <p>{application?.location}</p>
+        <p>{application?.salary}</p>
+        <p>{application?.description}</p>
+
+        <p>
+          <a
+            href={application?.url}
+            target="blank"
+            className={'text-blue-500 underline'}
+          >
+            {application?.url}
+          </a>
+        </p>
+
+        <h2 className="mb-3 mt-3 text-s font-bold">Events:</h2>
+
+        <EventsPanel events={application?.userEvents || []} />
       </>
     </Layout>
   );
