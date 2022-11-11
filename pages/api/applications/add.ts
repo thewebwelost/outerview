0716d87b783197147261f944af443c9e75611bfd
thewebwelost <thawebwelost@gmail.com>
2022-11-10 18:47:23 -0800

    application written to db

--- pages/api/applications/add.ts
+++ pages/api/applications/add.ts
@@ -2,14 +2,32 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../prisma/client';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const body = JSON.parse(req.body);
+  console.log('body', req.body);
 
-  if (!body.userId) {
+  if (!req.body.userId) {
     return res.status(400).send({ error: 'Unknown userId' });
   }
 
   try {
-    const applications = await prisma.application.create(req.body);
+    const { contacts, ...body } = req.body;
+
+    const applications = await prisma.application.create({
+      data: {
+        userId: body.userId,
+        company: body.title,
+        role: body.role,
+        url: body.url,
+        description: body.desc,
+        salary: body.comp,
+        location: body.location,
+        contacts: {
+          create: contacts.map((item: any) => ({
+            title: item.name,
+            url: item.contact,
+          })),
+        },
+      },
+    });
     res.status(200).json(applications);
   } catch (err) {
     console.error(err);
