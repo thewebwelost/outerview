62bc7d26a521ff922ca1110f45ddc3dbb8585c31
thewebwelost <thawebwelost@gmail.com>
2022-11-16 19:10:10 -0800

    initial preview styling

--- app/(inner)/profiles/add/page.tsx
+++ app/(inner)/profiles/add/page.tsx
@@ -81,6 +81,10 @@ const AddProfile: NextPage = () => {
     setSocialsContact('');
   };
 
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
   const [hardSkills, setHardSkills] = useState('');
   const [hardSkillsList, setHardSkillsList] = useState<string[]>([]);
 
@@ -89,74 +93,94 @@ const AddProfile: NextPage = () => {
   // Step #2
 
   const renderForm = () => {
-    return (
-      <div className="p-5 mt-5 border">
-        <h2 className="text-xl font-bold mb-2">Introduce yourself first</h2>
+    switch (step) {
+      case 0:
+        return (
+          <div className="p-5 mt-5 border">
+            <h2 className="text-xl font-bold mb-2">Introduce yourself first</h2>
 
-        <div className="flex">
-          <TextInput title={'Name'} value={name} handler={setName} />
-          <TextInput title={'Role'} value={role} handler={setRole} />
-        </div>
+            <div className="flex">
+              <TextInput title={'Name'} value={name} handler={setName} />
+              <TextInput title={'Role'} value={role} handler={setRole} />
+            </div>
 
-        <div className="flex">
-          <TextInput title={'Email'} value={email} handler={setEmail} />
-          <TextInput
-            title={'Location'}
-            value={location}
-            handler={setLocation}
-          />
-        </div>
+            <div className="flex">
+              <TextInput title={'Email'} value={email} handler={setEmail} />
+              <TextInput
+                title={'Location'}
+                value={location}
+                handler={setLocation}
+              />
+            </div>
 
-        <TextInput title={'Website'} value={website} handler={setWebsite} />
+            <TextInput title={'Website'} value={website} handler={setWebsite} />
 
-        <div>
-          <TextInput
-            title={'Socials title'}
-            value={socialsTitle}
-            handler={setSocialsTitle}
-          />
-          <TextInput
-            title={'Socials Contact'}
-            value={socialsContact}
-            handler={setSocialsContact}
-          />
+            <div>
+              <TextInput
+                title={'Socials title'}
+                value={socialsTitle}
+                handler={setSocialsTitle}
+              />
+              <TextInput
+                title={'Socials Contact'}
+                value={socialsContact}
+                handler={setSocialsContact}
+              />
 
-          <button
-            className="mt-2 text-white bg-blue-500"
-            onClick={addSocial}
-            type="button"
-          >
-            + add
-          </button>
-        </div>
+              <button
+                className="mt-2 text-white bg-blue-500"
+                onClick={addSocial}
+                type="button"
+              >
+                + add
+              </button>
+            </div>
 
-        <label htmlFor="summary" className="block mt-3 capitalize">
-          Summary
-          <textarea
-            className="block w-full px-2 py-1 border border-gray-200"
-            value={summary}
-            onChange={(e) => setSummary(e.target.value)}
-          />
-        </label>
+            <label htmlFor="summary" className="block mt-3 capitalize">
+              Summary
+              <textarea
+                className="block w-full px-2 py-1 border border-gray-200"
+                value={summary}
+                onChange={(e) => setSummary(e.target.value)}
+              />
+            </label>
 
-        <div>
-          <TextInput title={'Details'} value={details} handler={setDetails} />
+            <div>
+              <TextInput
+                title={'Details'}
+                value={details}
+                handler={setDetails}
+              />
 
-          <button
-            className="mt-2 text-white bg-blue-500"
-            onClick={addDetail}
-            type="button"
-          >
-            + add
-          </button>
-        </div>
+              <button
+                className="mt-2 text-white bg-blue-500"
+                onClick={addDetail}
+                type="button"
+              >
+                + add
+              </button>
+            </div>
 
-        <hr />
-        {/* <button className="text-white bg-blue-500" onClick={handleNextStep}>
-          Next
-        </button> */}
-      </div>
-    );
+            <hr />
+            <button className="text-white bg-blue-500" onClick={handleNextStep}>
+              Next
+            </button>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="p-5 mt-5 border">
+            <h2 className="text-xl font-bold mb-2">Experience</h2>
+            <hr />
+            <button className="text-white bg-blue-500" onClick={handleNextStep}>
+              Next
+            </button>
+          </div>
+        );
+
+      default:
+        break;
+    }
   };
 
   return (
@@ -164,31 +188,40 @@ const AddProfile: NextPage = () => {
       <h1 className="text-3xl font-bold underline">New Profile</h1>
 
       <div className="flex justify-between">
-        <div>{renderForm()}</div>
-        <div className="border font-bold">
-          <p className="text-2xl">{name || 'Your Name'}</p>
-          <p className="font-light italic">
-            {role || 'role'}, {location || 'location'}
-          </p>
-          <p className="font-light text-blue-500 underline">{website}</p>
-          <p className="font-light text-blue-500">{email}</p>
-          <p>Summary: {summary}</p>
+        <div className="basis-2/5">{renderForm()}</div>
 
-          <ul>
-            {detailsList.map((item, i) => (
-              <li key={i}>{item}</li>
-            ))}
-          </ul>
+        <div className="basis-3/5 p-5 border font-bold">
+          <span className="text-right">
+            <p className="text-2xl">{name || 'Your Name'}</p>
+            <p className="font-light italic">
+              {role || 'role'}, {location || 'location'}
+            </p>
+            <p className="font-light text-blue-500 underline">{website}</p>
+            <p className="font-light text-blue-500">{email}</p>
+          </span>
 
-          <ul>
+          <h2 className="text-xl mt-3 mb-1">Socials</h2>
+          <ul className="flex">
             {socialsList.map((item, i) => (
-              <li key={i}>
-                <a href={item.contact} target="_blank" rel="noreferrer">
-                  {item.title}
-                </a>
+              <li key={i} className="mr-2 text-blue-400">
+                {item.title}
               </li>
             ))}
           </ul>
+
+          <h2 className="text-xl mt-3 mb-1">Experience</h2>
+          <p className="font-light ">{summary}</p>
+
+          <h2 className="text-xl mt-3 mb-1">Details</h2>
+          <ul className="pl-5 list-disc ">
+            {detailsList.map((item, i) => (
+              <li key={i} className="font-light">
+                {item}
+              </li>
+            ))}
+          </ul>
+
+          <h2 className="text-xl mt-3 mb-1">Summary</h2>
         </div>
       </div>
     </>
