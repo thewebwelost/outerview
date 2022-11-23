5a5b3ba59bbf7b59150afd5af7b382c07b2025bd
thewebwelost <thawebwelost@gmail.com>
2022-11-22 16:35:55 -0800

    Personal Info form extracted

--- app/(inner)/profiles/add/page.tsx
+++ app/(inner)/profiles/add/page.tsx
@@ -3,6 +3,7 @@
 import type { NextPage } from 'next';
 import { SetStateAction, useState } from 'react';
 import TextInput from '../../../../components/atoms/TextInput';
+import PersonalInfo from '../../../../components/PersonalInfo';
 
 const defaultNewProfileFormState = {
   name: '',
@@ -34,7 +35,7 @@ interface IExperience {
 
 interface IEducation {}
 
-interface IProfileForm {
+export interface IProfileForm {
   name: string;
   role: string;
   summary: string;
@@ -207,110 +208,6 @@ const AddProfile: NextPage = () => {
   const handlePrevStep = () => {
     if (step > 0) setStep(step - 1);
   };
-
-  const PersonalInfo: React.FC<{
-    formState: IProfileForm;
-    setFormState: React.Dispatch<React.SetStateAction<IProfileForm>>;
-  }> = ({ formState, setFormState }) => (
-    <div className="p-5 mt-5 border">
-      <h2 className="text-xl font-bold mb-2">Introduce yourself first</h2>
-
-      <div className="w-full flex justify-between">
-        <TextInput
-          title={'Name'}
-          value={formState.name}
-          handler={(name: string) =>
-            setFormState({
-              ...formState,
-              name,
-            })
-          }
-        />
-        <TextInput
-          title={'Role'}
-          value={formState.role}
-          handler={(role: string) =>
-            setFormState({
-              ...formState,
-              role,
-            })
-          }
-        />
-      </div>
-
-      <div className="w-full flex justify-between">
-        <TextInput
-          title={'Email'}
-          value={formState.email}
-          handler={(email: string) =>
-            setFormState({
-              ...formState,
-              email,
-            })
-          }
-        />
-        <TextInput
-          title={'Location'}
-          value={formState.location}
-          handler={(location: string) =>
-            setFormState({
-              ...formState,
-              location,
-            })
-          }
-        />
-      </div>
-
-      <TextInput
-        title={'Website'}
-        value={formState.website}
-        handler={(website: string) =>
-          setFormState({
-            ...formState,
-            website,
-          })
-        }
-      />
-
-      <div>
-        <TextInput
-          title={'Socials title'}
-          value={formState.socialsTitle}
-          handler={(socialsTitle: string) =>
-            setFormState({
-              ...formState,
-              socialsTitle,
-            })
-          }
-        />
-
-        <TextInput
-          title={'Socials Contact'}
-          value={formState.socialsContact}
-          handler={(socialsContact: string) =>
-            setFormState({
-              ...formState,
-              socialsContact,
-            })
-          }
-        />
-
-        <button
-          className="mt-2 text-white bg-blue-500"
-          onClick={addSocial}
-          type="button"
-        >
-          + add
-        </button>
-      </div>
-
-      <div className="flex justify-end mt-10">
-        <button className="p-1 text-white bg-blue-500" onClick={handleNextStep}>
-          Next
-        </button>
-      </div>
-    </div>
-  );
 
   const ProfessionalInfo = () => (
     <div className="p-5 mt-5 border">
@@ -545,7 +442,12 @@ const AddProfile: NextPage = () => {
     switch (step) {
       case 0:
         return (
-          <PersonalInfo formState={formState} setFormState={setFormState} />
+          <PersonalInfo
+            formState={formState}
+            setFormState={setFormState}
+            addSocial={addSocial}
+            handleNextStep={handleNextStep}
+          />
         );
       case 1:
         return <ProfessionalInfo />;
---
+++ components/PersonalInfo.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { IProfileForm } from '../app/(inner)/profiles/add/page';
+import TextInput from './atoms/TextInput';
+
+interface IPersonalInfo {
+  formState: IProfileForm;
+  setFormState: React.Dispatch<React.SetStateAction<IProfileForm>>;
+  addSocial: () => void;
+  handleNextStep: () => void;
+}
+
+const PersonalInfo: React.FC<IPersonalInfo> = ({
+  formState,
+  setFormState,
+  addSocial,
+  handleNextStep,
+}) => {
+  return (
+    <div className="p-5 mt-5 border">
+      <h2 className="text-xl font-bold mb-2">Introduce yourself first</h2>
+
+      <div className="w-full flex justify-between">
+        <TextInput
+          title={'Name'}
+          value={formState.name}
+          handler={(name: string) =>
+            setFormState({
+              ...formState,
+              name,
+            })
+          }
+        />
+        <TextInput
+          title={'Role'}
+          value={formState.role}
+          handler={(role: string) =>
+            setFormState({
+              ...formState,
+              role,
+            })
+          }
+        />
+      </div>
+
+      <div className="w-full flex justify-between">
+        <TextInput
+          title={'Email'}
+          value={formState.email}
+          handler={(email: string) =>
+            setFormState({
+              ...formState,
+              email,
+            })
+          }
+        />
+        <TextInput
+          title={'Location'}
+          value={formState.location}
+          handler={(location: string) =>
+            setFormState({
+              ...formState,
+              location,
+            })
+          }
+        />
+      </div>
+
+      <TextInput
+        title={'Website'}
+        value={formState.website}
+        handler={(website: string) =>
+          setFormState({
+            ...formState,
+            website,
+          })
+        }
+      />
+
+      <div>
+        <TextInput
+          title={'Socials title'}
+          value={formState.socialsTitle}
+          handler={(socialsTitle: string) =>
+            setFormState({
+              ...formState,
+              socialsTitle,
+            })
+          }
+        />
+
+        <TextInput
+          title={'Socials Contact'}
+          value={formState.socialsContact}
+          handler={(socialsContact: string) =>
+            setFormState({
+              ...formState,
+              socialsContact,
+            })
+          }
+        />
+
+        <button
+          className="mt-2 text-white bg-blue-500"
+          onClick={addSocial}
+          type="button"
+        >
+          + add
+        </button>
+      </div>
+
+      <div className="flex justify-end mt-10">
+        <button className="p-1 text-white bg-blue-500" onClick={handleNextStep}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
