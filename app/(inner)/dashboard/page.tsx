b820659bcded2afa723882264f1b156f315efdfa
thewebwelost <thawebwelost@gmail.com>
2022-11-27 13:53:23 -0800

    minor copy changes

--- app/(inner)/dashboard/page.tsx
+++ app/(inner)/dashboard/page.tsx
@@ -45,15 +45,13 @@ const Dashboard = async () => {
 
   return (
     <>
-      <h2 className="mt-5 text-m text-gray-400 font-light">
-        Your applications:
-      </h2>
+      <h2 className="mt-5 text-m text-gray-400 font-light">Applications:</h2>
       <ApplicationsPanel applications={dashboard?.applications || []} />
 
-      <h2 className="mt-5 text-m text-gray-400 font-light">Upcoming events:</h2>
+      <h2 className="mt-5 text-m text-gray-400 font-light">Events:</h2>
       <EventsPanel events={dashboard?.events || []} />
 
-      <h2 className="mt-5 text-m text-gray-400 font-light">Your profiles:</h2>
+      <h2 className="mt-5 text-m text-gray-400 font-light">Profiles:</h2>
       <ProfilePanel profiles={dashboard?.profiles || []} />
     </>
   );
