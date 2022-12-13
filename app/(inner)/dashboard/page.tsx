b1e50dab885967323afa921d17209ce641f5eece
thewebwelost <thawebwelost@gmail.com>
2022-12-13 14:35:21 -0800

    cleanup

--- app/(inner)/dashboard/page.tsx
+++ app/(inner)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import ApplicationsPanel from '../../../components/ApplicationsPanel';
 import EventsPanel from '../../../components/EventsPanel';
 import ProfilePanel from '../../../components/ProfilePanel';
+import Spinner from '../../../components/Spinner';
 
 import type { IProfile } from '../../../components/Profile';
 import type { IUserEvent } from '../../../components/UserEvent';
@@ -11,7 +12,6 @@ import type { IApplication } from '../../../components/Application';
 import fetchDashboard from '../../../api/fetchDashboard';
 
 import { useQuery } from '@tanstack/react-query';
-import Spinner from '../../../components/Spinner';
 
 export interface IDashboard {
   id: number;
